922d6dfd3466426f0f49dc761ab6838a5fab74f8
sachin198 <55353653+sachin198@users.noreply.github.com>
2023-10-27 10:10:50 +0530

    Update add-sub.py
    update

--- sample1/add-sub.py
+++ sample1/add-sub.py
@@ -2,5 +2,5 @@ def calculation(x,y):
     addition = x+y
     subtraction = x-y
     return addition,subtraction
-c,d = calculation(20,10)
+c,d = calculation(20,20)
 print("Addition and subtraction number is: ",c,d)
